ccf137b0fa6a842621c7165f30717cafb122f6fc
Bruno Morais <brunosmmm@gmail.com>
2018-09-21 13:59:44 -0400

    actually fix the fix

--- dom0/local/scripts/fix-touchscreen.sh
+++ dom0/local/scripts/fix-touchscreen.sh
@@ -1,5 +1,7 @@
 # fix touchscreen: assign input to proper screen only
+stylus_name=$(xinput list --name-only | grep "Wacom.*stylus")
+eraser_name=$(xinput list --name-only | grep "Wacom.*eraser")
 xinput --map-to-output $(xinput list --id-only "ELAN Touchscreen") eDP1
-xinput --map-to-output $(xinput list --id-only "Wacom ISDv4 EC Pen stylus") eDP1
-xinput --map-to-output $(xinput list --id-only "Wacom ISDv4 EC Pen eraser") eDP1
+xinput --map-to-output $(xinput list --id-only "$stylus_name") eDP1
+xinput --map-to-output $(xinput list --id-only "$eraser_name") eDP1
 
