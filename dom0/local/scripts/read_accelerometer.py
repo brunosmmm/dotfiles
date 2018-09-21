38c5ed32cdb55eda3bb4fc79d794156010266a8b
Bruno Morais <brunosmmm@gmail.com>
2018-09-21 18:04:41 -0400

    update rotate script

--- dom0/local/scripts/read_accelerometer.py
+++ dom0/local/scripts/read_accelerometer.py
@@ -3,32 +3,36 @@ from glob import glob
 import sys
 from os import path
 
-def bdopen(fname):
+def bdopen(basedir, fname):
     return open(path.join(basedir, fname))
 
-def read_iio(fname):
-    return bdopen(fname).read()
+def read_iio(basedir, fname):
+    return bdopen(basedir, fname).read()
 
-def read_accel(fp):
+def read_accel(scale, fp):
     fp.seek(0)
     return float(fp.read())*scale
 
 def read_xy():
     for basedir in glob('/sys/bus/iio/devices/iio:device*'):
-        if 'accel' in read_iio('name'):
+        if 'accel' in read_iio(basedir, 'name'):
             break
     else:
         return (None, None)
 
-    scale = float(read_iio('in_accel_scale'))
+    scale = float(read_iio(basedir, 'in_accel_scale'))
 
-    accel_x = bdopen('in_accel_x_raw')
-    accel_y = bdopen('in_accel_y_raw')
-    x = read_accel(accel_x)
-    y = read_accel(accel_y)
+    accel_x = bdopen(basedir, 'in_accel_x_raw')
+    accel_y = bdopen(basedir, 'in_accel_y_raw')
+    x = read_accel(scale, accel_x)
+    y = read_accel(scale, accel_y)
     return (x, y)
 
 if __name__ == "__main__":
+    x, y = read_xy()
+    if x is None or y is None:
+        print('Accelerometer not found')
+        exit(1)
     print(x)
     print(y)
 
