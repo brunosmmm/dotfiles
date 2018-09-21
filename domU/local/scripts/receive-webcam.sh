39cdc8ff37fe262438415dadf9f5eaa806365368
Bruno Morais <brunosmmm@gmail.com>
2018-09-21 14:20:14 -0400

    update webcam scripts

--- domU/local/scripts/receive-webcam.sh
+++ domU/local/scripts/receive-webcam.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 read size pixfmt
+# setup loopback
 /home/user/.local/scripts/setup-v4l2.sh
+if [ $? -ne 0 ]; then
+  echo "Error setting up V4L2 loopback"
+  exit 1
+fi
 ffmpeg -s "$size" -f rawvideo -pix_fmt "$pixfmt" -i - -f v4l2 /dev/video0
 
 /home/user/.local/scripts/deinit-v4l2.sh
--- domU/local/scripts/setup-v4l2.sh
+++ domU/local/scripts/setup-v4l2.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+loopback_module=/home/user/.local/scripts/v4l2loopback.ko
+
+
+# load module if not loaded
 /usr/sbin/lsmod | grep v4l2loopback
 if [ $? -ne 0 ]; then
+  if [ ! -f $loopback_module ]; then
+    echo "Loopback module not found"
+    exit 1
+  fi
   sudo modprobe videodev
-  sudo insmod /home/user/.local/scripts/v4l2loopback.ko exclusive_caps=Y
+  sudo insmod $loopback_module exclusive_caps=Y
 fi
