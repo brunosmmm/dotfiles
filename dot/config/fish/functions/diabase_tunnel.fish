55387e1dfe589d7845f97da4b28f5b52afb0e357
Bruno Morais <brunosmmm@gmail.com>
2020-01-20 13:21:53 -0500

    update dotfiles

--- dot/config/fish/functions/diabase_tunnel.fish
+++ dot/config/fish/functions/diabase_tunnel.fish
@@ -6,11 +6,13 @@ function diabase_tunnel -a cmd -d "Enable ssh tunnel to diabase"
                 return 1
             end
             ssh -q -N -f -S /tmp/diabase-sock -L 9999:10.75.15.172:22 bmorais@gateway.coe.neu.edu
+            sshfs -p 9999 -o IdentityFile=~/.ssh/id_rsa bmorais@localhost:/home/bmorais /home/bruno/diabase
         case disable
             if not test -e /tmp/diabase-sock
                 echo "tunnel is not up, error"
                 return 1
             end
+            umount /home/bruno/diabase
             ssh -q -S /tmp/diabase-sock -O exit bmorais@localhost 2>&1 1> /dev/null
         case status
             if test -e /tmp/diabase-sock
