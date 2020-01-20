6dc2e3403f6bfa7412ba75e67d014290ed5dd2ff
Bruno Morais <brunosmmm@gmail.com>
2020-01-20 12:58:57 -0500

    use decorator to check files, add call to git diff

--- scripts/dotutils/misc.py
+++ scripts/dotutils/misc.py
@@ -4,21 +4,31 @@ import os
 import subprocess
 
 
+def _check_input_files(fn):
+    """Check input files."""
+
+    def wrapper(path_a, path_b):
+        """Verify."""
+        if not os.path.exists(path_a):
+            raise RuntimeError('file "{}" does not exist'.format(path_a))
+
+        if not os.path.exists(path_b):
+            raise RuntimeError('file "{}" does not exist'.format(path_b))
+
+        if not os.path.isfile(path_a):
+            raise RuntimeError('"{}" is not a file'.format(path_a))
+
+        if not os.path.isfile(path_b):
+            raise RuntimeError('"{}" is not a file'.format(path_b))
+
+        return fn(path_a, path_b)
+
+    return wrapper
+
+
+@_check_input_files
 def files_differ(path_a, path_b):
     """Check whether files differ."""
-
-    if not os.path.exists(path_a):
-        raise RuntimeError('file "{}" does not exist'.format(path_a))
-
-    if not os.path.exists(path_b):
-        raise RuntimeError('file "{}" does not exist'.format(path_b))
-
-    if not os.path.isfile(path_a):
-        raise RuntimeError('"{}" is not a file'.format(path_a))
-
-    if not os.path.isfile(path_b):
-        raise RuntimeError('"{}" is not a file'.format(path_b))
-
     try:
         subprocess.check_call(
             ["diff", "-q", path_a, path_b],
@@ -33,3 +43,20 @@ def files_differ(path_a, path_b):
             raise RuntimeError("diff failed")
 
     return False
+
+
+@_check_input_files
+def git_diff(path_a, path_b):
+    """Get git diff output."""
+    try:
+        ret = subprocess.run(
+            ["git", "diff", path_a, path_b],
+            stderr=subprocess.DEVNULL,
+            stdout=subprocess.PIPE,
+            check=True,
+        )
+    except subprocess.CalledProcessError as ex:
+        if ex.returncode == 1:
+            return ex.stdout.decode()
+        else:
+            raise RuntimeError("diff failed")
