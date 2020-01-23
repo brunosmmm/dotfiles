c7ef1b53a600ed26350bb47b326a65d0ea97199e
Bruno Morais <brunosmmm@gmail.com>
2020-01-22 19:46:41 -0500

    remove emacs.d

--- dot/emacs.d/private/bruno_other/funcs.el
+++
@@ -1,8 +0,0 @@
-
-(defun spacemacs/insert-latex-keychords ()
-  "Insert keychord combinations"
-  (progn
-    (key-chord-mode 1)
-    (key-chord-define-local "xb" 'latex/font-bold)
-    (key-chord-define-local "xe" 'latex/font-emphasis)
-    (key-chord-define-local "xc" 'latex/font-code)))
