029d1deb721d19ae9bb1ba6a3b866457f977abab
Bruno Morais <brunosmmm@gmail.com>
2020-01-24 11:54:11 -0500

    much better polybar configuration

--- dot/config/polybar/launch.sh
+++ dot/config/polybar/launch.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
 ## Add this to your wm startup file.
+configfile=~/.config/polybar/config.ini
+polybar=$(which polybar)
+# polybar=~/work/polybar/build/bin/polybar
 
 # Terminate already running bar instances
 killall -q polybar
@@ -8,6 +11,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
-polybar -c ~/.config/polybar/config.ini secondary &
+# launch multiple bars
+for monitor in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    if [ $monitor == "eDP1" ]; then
+        MONITOR=$monitor $polybar -c $configfile main &
+    else
+        MONITOR=$monitor $polybar -c $configfile secondary &
+    fi
+done
