2e8e0f3ea573bd547ff6ac4655100b8c14bbc662
Bruno Morais <brunosmmm@gmail.com>
2020-03-07 12:19:17 -0500

    Update dotfiles

--- dot/config/fish/functions/dotfiles-update.fish
+++ dot/config/fish/functions/dotfiles-update.fish
@@ -1,6 +1,5 @@
 function dotfiles-update -a opt -d "Update dotfiles from home directory."
     set dotfilepath (project-get-path dotfiles)
     $dotfilepath/scripts/dotupdate "$dotfilepath/dot" update --interactive
-    set hostname (hostname)
     $dotfilepath/scripts/dotupdate "$dotfilepath/dot.$hostname" update --interactive
 end
