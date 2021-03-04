7203c53c1abb8c8ac3524ecdd601969dee427594
Bruno <brunosmmm@gmail.com>
2021-03-04 15:17:24 -0500

    Update dotfiles

--- dot.morais-Desktop/config/fish/conf.d/bruno.fish
+++ dot.morais-Desktop/config/fish/conf.d/bruno.fish
@@ -1,4 +1,6 @@
 
 set -x PROJECTS_FILE "$HOME/.config/projects/projects.json"
 set -x CREDENTIALS_FILE "$HOME/.config/projects/credentials.json"
+set -x BOOKMARKS_FILE "$HOME/.config/projects/bookmarks.json"
 set -g theme_date_timezone America/New_York
+set -U fish_user_paths /home/bruno/.local/bin $fish_user_paths
--- dot/config/fish/functions/credentials-get.fish
+++ dot/config/fish/functions/credentials-get.fish
@@ -1,23 +1,35 @@
 function credentials-get -a what -d "Get credentials for a key"
     if test -z "$what"
-        return
+        return -1
     end
 
     if not set -q CREDENTIALS_FILE
-        return
+        return -1
     end
 
     if not test -f "$CREDENTIALS_FILE"
-        return
+        return -1
     end
 
     set user (cat "$CREDENTIALS_FILE" | jq ."$what".user -r)
     set pass (cat "$CREDENTIALS_FILE" | jq ."$what".pass -r)
+    set pass_type (cat "$CREDENTIALS_FILE" | jq ."$what".pass_type -r)
+    if test "$pass_type" = "null"
+        return -1
+    end
     if test "$user" = "null"
-        return
+        return -1
     end
     if test "$pass" = "null"
-        return
+        return -1
     end
-    echo "$user:$pass"
+
+    if test "$pass_type" = "text"
+        echo "$user:$pass"
+    else
+        if test "$pass_type" = "pass"
+            set pass (pass "$pass")
+            echo "$user:$pass"
+        end
+    end
 end
