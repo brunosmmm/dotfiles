b3eb6e4e0581ea4aaed0eb10d33994c48fcd3c63
Bruno Morais <brunosmmm@gmail.com>
2021-08-31 18:20:50 -0400

    Update dotfiles

--- dot.bruno-yoga9i/config/fish/conf.d/bruno.fish
+++ dot.bruno-yoga9i/config/fish/conf.d/bruno.fish
@@ -11,14 +11,14 @@ set -x BOOKMARKS_FILE "$HOME/.config/bookmarks/bookmarks.json"
 
 # paths
 # set -x GOPATH /home/bruno/.go
-set -gx PATH /home/bruno/.local/bin:/home/bruno/.gem/ruby/2.6.0/bin:/home/bruno/.yarn/bin $PATH
+# set -gx PATH /home/bruno/.local/bin:/home/bruno/.gem/ruby/2.6.0/bin:/home/bruno/.yarn/bin $PATH
 
 # editor
 set -x EDITOR (which vim)
 
 # aliases
 alias ls exa
-alias cat bat
+# alias cat bat
 # alias ssh "env TERM=xterm-256color ssh"
 
 set -U FZF_LEGACY_KEYBINDINGS 0
